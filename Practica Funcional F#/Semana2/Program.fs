004437cec932bc55ea54208dac282a619ddb64de
René Sánchez <rsanchez@Renes-MacBook-Air.local>
2023-09-16 12:25:04 -0600

    Primer Archivo Practica F#

---
+++ Practica Funcional F#/Semana2/Program.fs
@@ -0,0 +1,9 @@
+﻿open Recipientes
+open RutaCorta
+
+let pasosRecipientes = prof [2; 2] [2; 0]
+printfn "%A" pasosRecipientes
+
+let ruta = prof2 "i" "f" grafo
+printfn "%A" ruta
+0 // Salida del programa
---
+++ Practica Funcional F#/Semana2/RutaCorta.fs
@@ -0,0 +1,43 @@
+﻿module RutaCorta
+
+open Recipientes
+// Grafo de prueba
+let grafo = [
+    ("i", ["a"; "b"]);
+    ("a", ["i"; "c"; "d"]);
+    ("b", ["i"; "c"; "d"]);
+    ("c", ["a"; "b"; "x"]);
+    ("d", ["a"; "b"; "f"]);
+    ("f", ["d"]);
+    ("x", ["c"])
+]
+
+
+// Función para generar vecinos
+let rec vecinos nodo grafo =
+    match grafo with
+    | [] -> []
+    | (head, neighbors) :: rest ->
+        if head = nodo then neighbors
+        else vecinos nodo rest
+
+// Función para extender una ruta
+let extender ruta grafo = 
+    List.filter
+        (fun x -> x <> [])
+        (List.map  (fun x -> if (miembro x ruta) then [] else x::ruta) (vecinos (List.head ruta) grafo)) 
+
+// Función principal de búsqueda en profundidad
+let rec prof2 ini fin grafo =
+    let rec prof_aux fin ruta grafo =
+        if List.isEmpty ruta then []
+        elif (List.head (List.head ruta)) = fin then
+            List.rev (List.head ruta) //:: prof_aux fin ((extender (List.head ruta) grafo) @ (List.tail ruta)) grafo       
+        else
+            prof_aux fin ((List.tail ruta) @ (extender (List.head ruta) grafo)  ) grafo
+    prof_aux fin [[ini]] grafo
+
+
+
+        
+    
