fc0d75f43656b9f94077ada1be0ab9cde6d11f12
René Sánchez <rsanchez@Renes-MacBook-Air.local>
2023-11-10 14:34:46 -0600

    update

--- Ejemplos Agenda/Agenda/src/Data/ObjAgenda.java
+++
@@ -1,8 +0,0 @@
-package Data;
-
-import java.util.LinkedList;
-
-public interface ObjAgenda {
-    public abstract void imprimir();
-    public abstract void showGUI(LinkedList<Object> laAgenda);
-}
