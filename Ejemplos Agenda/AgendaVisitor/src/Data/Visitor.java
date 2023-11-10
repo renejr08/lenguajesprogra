17dd5610afa58803f62ebe9400af4317eb6af3fe
René Sánchez <rsanchez@Renes-MacBook-Air.local>
2023-11-10 14:30:14 -0600

    Update

--- Ejemplos Agenda/AgendaVisitor/src/Data/Visitor.java
+++
@@ -1,7 +0,0 @@
-package Data;
-
-public interface Visitor<T> {
-    public T visitContactoFamiliar(ContactoFamiliar ctx);
-    public T visitEventoReunion(EventoReunion ctx);
-    //cualquier otra clase nueva en la jerarquía debe visitarse
-}
